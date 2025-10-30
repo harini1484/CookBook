a1e6a14012cec318ed5be0aa5edbe3e72a8b8921
Harini <harini140804@gmail.com>
2025-10-30 12:46:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-<<<<<<< HEAD
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
-
-import Home from './pages/Home';
-import Category from './pages/Category';
-import Recipie from './pages/Recipie';
-=======
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
-import React from "react";
 
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Recipie from "./pages/Recipie";
->>>>>>> e6429f2a7eb0c4a7ec9f78a1d3502bdc9f984ca2
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     <Navbar />
-     
-     <Routes>
-
-      <Route path="/" element={<Home />} />
-      <Route path="/category/:id" element={<Category />} />
-      <Route path="/recipie/:id" element={<Recipie />} />
-     </Routes>
-
-     <Footer />
-=======
       <Navbar />
 
       <Routes>
@@ -43,7 +20,6 @@ function App() {
       </Routes>
 
       <Footer />
->>>>>>> e6429f2a7eb0c4a7ec9f78a1d3502bdc9f984ca2
     </div>
   );
 }
